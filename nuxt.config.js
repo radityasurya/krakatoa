6fc46c381373bb0f9fe1ba30c5abe6ebed40e8c2
radityasurya <radityasurya@hotmail.com>
2018-08-12 00:51:40 +0200

    Update config and packages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,33 +1,56 @@
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: "krakatoa",
+    title: "Krakatoa - Internet Marketing Platform",
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        charset: "utf-8"
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1"
+      },
       {
         hid: "description",
         name: "description",
         content: " Internet marketing project manager "
       }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/newfavicon.ico"
+      },
+      {
+        href:
+          "//cdn.materialdesignicons.com/2.0.46/css/materialdesignicons.min.css",
+        rel: "stylesheet"
+      }
+    ]
   },
-  css: ["normalize.css", "~/assets/style/app.scss"],
+  css: [
+    "element-ui/lib/theme-chalk/reset.css",
+    "element-ui/lib/theme-chalk/index.css",
+    "normalize.css",
+    "~/assets/style/app.scss"
+  ],
   modules: [["nuxt-sass-resources-loader", "~/assets/style/app.scss"]],
   /*
-  ** Customize the progress bar color
-  */
-  loading: { color: "#3B8070" },
+   ** Customize the progress bar color
+   */
+  loading: {
+    color: "#0396ff"
+  },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** Run ESLint on save
-    */
+     ** Run ESLint on save
+     */
     extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
@@ -37,6 +60,18 @@ module.exports = {
           exclude: /(node_modules)/
         });
       }
+    },
+    postcss: {
+      plugins: {
+        "postcss-custom-properties": false
+      }
     }
-  }
+  },
+  router: {
+    linkActiveClass: "active"
+  },
+  plugins: [
+    "@/plugins/element-ui"
+    // "@/plugins/buefy"
+  ]
 };
