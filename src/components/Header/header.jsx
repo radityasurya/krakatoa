3a1fde99aa9e53977a23957d35fa31ac8f5c4847
radityasurya <radityasurya@hotmail.com>
2018-10-22 00:33:22 +0200

    Add header component

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => (
+  <header>
+    <div className="header__container">
+      <div>
+        <input placeholder="Search" />
+      </div>
+    </div>
+  </header>
+);
+
+export default Header;
